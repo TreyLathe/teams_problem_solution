6295114faa58877bb45242f571af1c2c145dc2a2
WCIII <warrenlathe@me.com>
2024-02-01 11:43:05 -1000

    added stats test, fixed code

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ function formTeams(students) {
 
   // Helper function to find the next available team index
   function findNextAvailableTeam() {
-      for (let i = 0; i < teamCount; i++) {
-          if (teams[i].length < teamSizes[i]) {
-              return i;
-          }
+    for (let i = 0; i < teamCount; i++) {
+      if (teams[i].length < teamSizes[i]) {
+        return i;
       }
-      return -1; // No available team found
+    }
+    return -1; // No available team found
   }
 
   // Shuffle the students randomly to break ties
@@ -22,136 +22,236 @@ function formTeams(students) {
 
   // Iterate through students' preferences to assign mutually preferred students within their top three choices
   for (const student of students) {
-      if (assignedStudents.has(student.name)) {
-          // This student has already been assigned to a team, skip to the next student
-          continue;
+    if (assignedStudents.has(student.name)) {
+      // This student has already been assigned to a team, skip to the next student
+      continue;
+    }
+
+    // Initialize variables for preference index, assigned flag, and top three preferences
+    let preferenceIndex = 0; // Index to track the current preference being checked
+    let assigned = false; // Flag to indicate if the student has been assigned to a team
+    let topThreePreferences = student.preferences.slice(0, 3); // Get the top three preferences of the student
+    // Start a loop that continues until the student is assigned to a team or all top three preferences have been checked
+    // Loop through the top three preferences of the current student
+    while (!assigned && preferenceIndex < topThreePreferences.length) {
+      // Get the name of the preferred student at the current preference index
+      const preferredStudentName = topThreePreferences[preferenceIndex];
+      // Find the preferred student object from the students array
+      const preferredStudent = students.find((s) => s.name === preferredStudentName);
+
+      // Check if there is a preferred student and the preferred student has not been assigned yet
+      if (preferredStudent && !assignedStudents.has(preferredStudentName)) {
+        // Find the index of the next available team
+        const teamIndex = findNextAvailableTeam();
+
+        if (teamIndex !== -1) {
+          // Add the current student to the team
+          teams[teamIndex].push(student);
+          // Add the preferred student to the team
+          teams[teamIndex].push(preferredStudent);
+          // Mark the current student and the preferred student as assigned
+          assignedStudents.add(student.name);
+          assignedStudents.add(preferredStudentName);
+          // Set the assigned flag to true to break the loop
+          assigned = true;
+        }
       }
 
-      let preferenceIndex = 0;
-      let assigned = false;
-      let topThreePreferences = student.preferences.slice(0, 3); // Get the top three preferences
-
-      // Start a loop that continues until the student is assigned to a team or all top three preferences have been checked
-      while (!assigned && preferenceIndex < topThreePreferences.length) {
-          const preferredStudentName = topThreePreferences[preferenceIndex];
-          const preferredStudent = students.find((s) => s.name === preferredStudentName);
-
-          if (preferredStudent && !assignedStudents.has(preferredStudentName)) {
-              const teamIndex = findNextAvailableTeam();
-
-              if (teamIndex !== -1) {
-                  // Add the current student to the team
-                  teams[teamIndex].push(student);
-                  // Add the preferred student to the team
-                  teams[teamIndex].push(preferredStudent);
-                  // Mark the current student and the preferred student as assigned
-                  assignedStudents.add(student.name);
-                  assignedStudents.add(preferredStudentName);
-                  // Set the assigned flag to true to break the loop
-                  assigned = true;
-              }
-          }
-
-          preferenceIndex++;
-      }
+      preferenceIndex++;
+    }
   }
 
   // Now, let's assign the remaining unassigned students to the remaining teams based on their lower preferences
-  const remainingStudents = students.filter((student) => !assignedStudents.has(student.name));
+  const remainingStudents = students.filter(
+    (student) => !assignedStudents.has(student.name)
+  );
   let remainingTeamIndex = 0;
 
   // Iterate through the remaining students and assign them to the remaining teams
   for (const student of remainingStudents) {
-      const teamIndex = findNextAvailableTeam();
-      // Find the index of the next available team
-      if (teamIndex !== -1) {
-          teams[teamIndex].push(student);
-          assignedStudents.add(student.name);
-      } else {
-          // All teams are full, break the loop
-          break;
-      }
+    const teamIndex = findNextAvailableTeam();
+    // Find the index of the next available team
+    if (teamIndex !== -1) {
+      teams[teamIndex].push(student);
+      assignedStudents.add(student.name);
+    } else {
+      // All teams are full, break the loop
+      break;
+    }
   }
 
   return teams;
 }
 
+// Array of student names and their preferences
+const students = [
+  {
+    name: "Luke Skywalker",
+    preferences: [
+      "Master Yoda",
+      "Leia Organa",
+      "Han Solo",
+      "Obiwan Kenobi",
+      "R2D2",],
+  },
+  {
+    name: "Leia Organa",
+    preferences: [
+      "Han Solo", 
+      "C3PO", 
+      "Wicket", 
+      "Luke Skywalker", 
+      "Chewbacca"],
+  },
+  {
+    name: "Han Solo",
+    preferences: [
+      "Chewbacca",
+      "Leia Organa",
+      "Luke Skywalker",
+      "Lando Calrissian",
+      "Jek Porkins",
+    ],
+  },
+  {
+    name: "Master Yoda",
+    preferences: [
+      "Luke Skywalker",
+      "Obiwan Kenobi",
+      "Leia Organa",
+      "Mon Mothma",
+      "Wicket",
+    ],
+  },
+  {
+    name: "ObiWan Kenobi",
+    preferences: [
+      "Luke Skywalker",
+      "Master Yoda",
+      "Leia Organa",
+      "r2d2",
+      "Admiral Ackbar",
+    ],
+  },
+  {
+    name: "r2d2",
+    preferences: [
+      "c3po",
+      "Luke Skywalker",
+      "Leia Organa",
+      "Han Solo",
+      "Master Yoda",
+    ],
+  },
+  {
+    name: "c3po",
+    preferences: ["r2d2", "Luke Skywalker", "Han Solo", "Wicket", "Chewbacca"],
+  },
+  {
+    name: "Wicket",
+    preferences: [
+      "Chewbacca",
+      "Leia Organa",
+      "Luke Skywalker",
+      "Han Solo",
+      "r2d2",
+    ],
+  },
+  {
+    name: "Chewbacca",
+    preferences: [
+      "Han Solo",
+      "Luke Skywalker",
+      "Leia Organa",
+      "c3po",
+      "Wicket",
+    ],
+  },
+  {
+    name: "Admiral Ackbar",
+    preferences: [
+      "Mon Mothma",
+      "Leia Organa",
+      "Luke Skywalker",
+      "Han Solo",
+      "Master Yoda",
+    ],
+  },
+  {
+    name: "Darth Vader",
+    preferences: [
+      "Luke Skywalker",
+      "Leia Organa",
+      "Master Yoda",
+      "Mon Mothma",
+      "Admiral Ackbar",
+    ],
+  },
+  {
+    name: "Mon Mothma",
+    preferences: [
+      "Admiral Ackbar",
+      "Leia Organa",
+      "Master Yoda",
+      "Obiwan Kenobi",
+      "c3po",
+    ],
+  },
+  {
+    name: "Lando Calrissian",
+    preferences: [
+      "Han Solo",
+      "Chewbacca",
+      "Luke Skywalker",
+      "Leia Organa",
+      "r2d2",
+    ],
+  },
+  {
+    name: "Boba Fett",
+    preferences: [
+      "Jabbathe Hutt",
+      "Lando Calrissian",
+      "Darth Vader",
+      "Han Solo",
+      "c3po",
+    ],
+  },
+  {
+    name: "Jabbathe Hutt",
+    preferences: [
+      "Boba Fett",
+      "Leia Organa",
+      "Han Solo",
+      "Darth Vader",
+      "Luke Skywalker",
+    ],
+  },
+  {
+    name: "Jek Porkins",
+    preferences: [
+      "Luke Skywalker",
+      "Admiral Ackbar",
+      "Han Solo",
+      "Leia Organa",
+      "Mon Mothma",
+    ],
+  },
+];
 
-  // Array of student names and their preferences
-  const students = [
-    {
-              name: 'Luke Skywalker',
-              preferences: ['Master Yoda', 'Leia Organa', 'Han Solo', 'Obiwan Kenobi', 'R2D2'],
-            },
-            {
-              name: 'Leia Organa',
-              preferences: ['Han Solo', 'C3PO', 'Wicket', 'Luke Skywalker', 'Chewbacca'],
-            },
-            {
-                name: 'Han Solo',
-                preferences: ['Chewbacca', 'Leia Organa', 'Luke Skywalker', 'Lando Calrissian', 'Jek Porkins'],
-              },
-            {
-                name: 'Master Yoda',
-                preferences: ['Luke Skywalker', 'Obiwan Kenobi', 'Leia Organa', 'Mon Mothma', 'Wicket'],
-              },
-              {
-                name: 'ObiWan Kenobi',
-                preferences: ['Luke Skywalker', 'Master Yoda', 'Leia Organa', 'r2d2', 'Admiral Ackbar'],
-              },
-              {
-                name: 'r2d2',
-                preferences: ['c3po', 'Luke Skywalker', 'Leia Organa', 'Han Solo', 'Master Yoda'],
-              },
-              {
-                name: 'c3po',
-                preferences: ['r2d2', 'Luke Skywalker', 'Han Solo', 'Wicket', 'Chewbacca'],
-              },
-              {
-                name: 'Wicket',
-                preferences: ['Chewbacca', 'Leia Organa', 'Luke Skywalker', 'Han Solo', 'r2d2'],
-              },
-              {
-                name: 'Chewbacca',
-                preferences: ['Han Solo', 'Luke Skywalker', 'Leia Organa', 'c3po', 'Wicket'],
-              },
-              {
-                name: 'Admiral Ackbar',
-                preferences: ['Mon Mothma','Leia Organa', 'Luke Skywalker', 'Han Solo', 'Master Yoda'],
-              },
-              {
-                name: 'Darth Vader',
-                preferences: ['Luke Skywalker', 'Leia Organa', 'Master Yoda', 'Mon Mothma', 'Admiral Ackbar'],
-              },
-              {
-                name: 'Mon Mothma',
-                preferences: ['Admiral Ackbar', 'Leia Organa', 'Master Yoda', 'Obiwan Kenobi', 'c3po'],
-              },
-              {
-                name: 'Lando Calrissian',
-                preferences: ['Han Solo', 'Chewbacca', 'Luke Skywalker', 'Leia Organa', 'r2d2'],
-              },
-              {
-                name: 'Boba Fett',
-                preferences: ['Jabbathe Hutt', 'Lando Calrissian', 'Darth Vader', 'Han Solo', 'c3po'],
-              },
-              {
-                name: 'Jabbathe Hutt',
-                preferences: ['Boba Fett', 'Leia Organa', 'Han Solo', 'Darth Vader', 'Luke Skywalker'],
-              },
-              {
-                name: 'Jek Porkins',
-                preferences: ['Luke Skywalker', 'Admiral Ackbar', 'Han Solo', 'Leia Organa', 'Mon Mothma'],
-              },
-          ];
-  
-  // Form teams based on preferences
-  const teams = formTeams(students);
-  
-  // Print the resulting teams
-  for (let i = 0; i < teams.length; i++) {
-    console.log(`Team ${i + 1}:`);
-    for (const student of teams[i]) {
-      console.log(`- ${student.name}`);
-    }
+// Form teams based on preferences
+const teams = formTeams(students);
+
+// Print the resulting teams
+for (let i = 0; i < teams.length; i++) {
+  console.log(`Team ${i + 1}:`);
+  for (const student of teams[i]) {
+    console.log(`- ${student.name}`);
   }
+}
+
+// Export the formTeams and students  for testing
+module.exports = {
+  formTeams,
+  students
+};
---
+++ stats.js
@@ -0,0 +1,31 @@
+const { formTeams, students } = require('./server'); // Assuming formTeams is exported from server.js
+
+// Initialize a map to store the count of times each pair of students is on the same team
+const pairCounts = new Map();
+
+// Run the formTeams function 1,000 times
+for (let i = 0; i < 1000; i++) {
+  const studentsCopy = JSON.parse(JSON.stringify(students)); // Create a deep copy of the students array
+  const teams = formTeams(studentsCopy); // Pass the copy to the formTeams function
+
+  // For each team, update the count for each pair of students
+  for (const team of teams) {
+    for (let j = 0; j < team.length; j++) {
+      for (let k = j + 1; k < team.length; k++) {
+        // Sort the student names so that the pair is always in the same order
+        const pair = [team[j].name, team[k].name].sort().join('-');
+
+        // Update the count for this pair
+        pairCounts.set(pair, (pairCounts.get(pair) || 0) + 1);
+      }
+    }
+  }
+}
+
+// Sort the pairCounts map in descending order based on the count
+const sortedPairCounts = new Map([...pairCounts.entries()].sort((a, b) => b[1] - a[1]));
+
+// Print the statistics in descending order
+for (const [pair, count] of sortedPairCounts) {
+  console.log(`${pair}: ${count} times`);
+}
